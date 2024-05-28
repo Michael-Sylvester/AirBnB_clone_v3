044b4a31d58e73f556c95453f3494ecbfc473635
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-05-28 13:35:22 +0100

    task 4 use count methode to retrieve count of each obj

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,22 @@
 """Index module"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
+classes = {0: "amenities", 1: "cities", 2: "places",
+           3: "reviews", 4: "states", 5: "users"}
 
 
 @app_views.route('/status')
 def json_status():
     """Return satus of the api"""
     return jsonify({"status": "ok"})
+
+
+@app_views.route('/stats')
+def view_stats():
+    """Return the stats of the api"""
+    count_obj = {}
+    for index in range(0, len(classes)):
+        count_obj[classes[index]] = storage.count(classes[index])
+    return count_obj
