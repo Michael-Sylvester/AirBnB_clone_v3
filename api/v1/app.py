594f7eeb6eac5a52d2b61b706c38c57d06e0692f
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-05-28 03:33:35 +0100

    task 3: basic api setup

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module the runs the api"""
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask()
+app.register_blueprint(app_views)
+
+@app.route('/')
+def hello_world():
+    """"""
+    return 'Hello world'
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+if __name__ == '__main__':
+    import os
+
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', '5000')
+
+    app.run(host=host, port=int(port), threaded=True)
